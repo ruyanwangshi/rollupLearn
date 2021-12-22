19277938236181bb9ad6b5f87799047668447528
liudong <liudong-ls@bestpay.com.cn>
2021-12-22 18:29:10 +0800

    feat: 增加postcss插件

--- rollup.config.js
+++ rollup.config.js
@@ -2,23 +2,28 @@ import { terser } from 'rollup-plugin-terser'
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import typescriptplugin from '@rollup/plugin-typescript'
+import postcss from 'rollup-plugin-postcss'
 
 console.log(process.env.NODE_ENV);
 
 export default {
   input: 'src/index.ts',
   output: {
-    file: 'dist/bundle.js',
+    file: 'lib/bundle.js',
     name: 'piniaPlugin',
     format: 'umd',
   },
   plugins: [
+    postcss(), // 对样式进行转化
     typescriptplugin({ tsconfig: './tsconfig.json' }),
     babel({
       exclude: 'node_modules/**', // 防止打包node_modules下的文件
       runtimeHelpers: true, // 使plugin-transform-runtime生效
     }),
-    terser(),
-    commonjs(),
+    terser(), // 打包压缩插件
+    commonjs(), // 使用commonjs引入
   ],
+  external:[  //外部库， 使用'umd'文件时需要先引入这个外部库
+    'vue'
+  ]
 }
